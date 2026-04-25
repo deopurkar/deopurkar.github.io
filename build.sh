71ece4a04f1aa430a793b400c1e69e848f57c1cb
Anand Deopurkar <git@ananddeopurkar.org>
2026-04-25 22:04:12 +1000

    do not install latex for now

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 emacs -Q --batch \
-  --eval "(setq org-agenda-files nil org-id-track-globally nil)" \
+  --eval "(setq org-agenda-files nil org-id-track-globally nil noninteractive t)" \
   -l build.el \
   -f org-publish-all
